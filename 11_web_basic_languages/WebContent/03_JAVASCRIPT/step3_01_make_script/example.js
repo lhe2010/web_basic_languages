9d134b0831025ad124ad01485e46002505e1a458
User <User@201-11>
2020-12-02 17:44:41 +0900

    2020-12-02 수 자바스크립트 강의

---
+++ 11_web_basic_languages/WebContent/03_JAVASCRIPT/step3_01_make_script/example.js
@@ -0,0 +1,20 @@
+// javascript 파일확장자는 js이다. 
+
+//  화면에 찍음. 개행 안됨
+	document.write("<h1>Welcome to Javascript2</h1>");
+	document.write("Hello Javascript1");
+	document.write("Hello Javascript2");
+	document.write("Hello Javascript3");
+	document.write("<hr>");
+	
+// 	html이 이김. 개행이되도록 하려면 <pre>태그 써야함. 그래서 잘안씀
+	document.writeln("<h1>Welcome to Javascript2</h1>");
+	document.writeln("<pre>");
+	document.writeln("Hello Javascript1");
+	document.writeln("Hello Javascript2");
+	document.writeln("Hello Javascript3");
+	document.writeln("</pre>");
+	document.writeln("<hr>");
+	
+//  개발자도구 콘솔에서 볼수있음.
+	console.log("hello javascript2");
